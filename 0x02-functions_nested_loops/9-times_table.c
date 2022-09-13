06998c9c230186c956104c55b75d3f9c3eaff33d
ENAIRA2007 <egwaiseunorobosa@yahoo.com>
2022-09-13 15:33:34 -0700

    TASK 9 CORRECTION

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table, starting with 0.
+ *times_table - Prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
@@ -16,9 +16,9 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-																	prod = num * mult;
+			prod = num * mult;
 
-																	if (prod <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
