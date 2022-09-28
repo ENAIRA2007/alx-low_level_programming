7a76b5fe1f76dc8fc25c5fb882a797b2c17e2745
ENAIRA2007 <egwaiseunorobosa@yahoo.com>
2022-09-28 15:51:12 -0700

    TASK 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,9 +11,9 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
- if (*s == 0)
-  return (1);
- return (check_pal(s, 0, _strlen_recursion(s)));
+if (*s == 0)
+return (1);
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
